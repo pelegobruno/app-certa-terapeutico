3daf520c8da03588b04a485f5fed5837b2a5d358
Git"dev"brunno <pelegobruno@gmail.com>
2025-12-22 19:15:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = "certa-v1";
+const CACHE_NAME = "certa-v3"; // 🔴 MUDE ESSE NÚMERO SEMPRE QUE ATUALIZAR
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css",
-  "./script.js"
+  "./app.css",
+  "./app.js",
+  "./manifest.json",
+  "./ativos/imagens/certa-a-png.png",
+  "./ativos/fontes/Turtles.otf"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
